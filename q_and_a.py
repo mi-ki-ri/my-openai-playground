5d7f784bc5ad615558fcafec58f7ecb6b22418d0
HOTTA Tatsuya <hotta.tatsuya.fcc@gmail.com>
2023-02-13 15:09:35 +0900

    q and a

--- q_and_a.py
+++ q_and_a.py
@@ -8,7 +8,7 @@ def main():
   args= parser.parse_args()
   TEXT = args.text
 
-  MY_PROMPT = f"""I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with "Unknown".
+  MY_PROMPT = f"""I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer, in Japanese. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with "Unknown".
 
   Q: What is human life expectancy in the United States?
   A: Human life expectancy in the United States is 78 years.
